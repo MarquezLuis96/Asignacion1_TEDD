db6c8c0500e45e305e4a520af079060caf24dd27
Luis Marquez <englamc@gmail.com>
2021-12-13 15:29:25 -0400

    Setting-up project and modifying .gitignore

---
+++ Logic/Source.cpp
@@ -0,0 +1,16 @@
+/*
+* Authors:
+*	Carlos Pedraza
+*	Luis Marquez
+*/
+
+//Headers
+#include <iostream>
+
+//Name spaces
+using namespace std;
+
+//Main function
+int main(int args, char* argsv[]) {
+	return 0;
+}
