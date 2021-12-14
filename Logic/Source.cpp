c739bc32baf1f6cd8a837b87d6a502841e301e5f
Luis Marquez <englamc@gmail.com>
2021-12-14 16:34:45 -0400

    Adding sigmaArrayElements

--- Logic/Source.cpp
+++ Logic/Source.cpp
@@ -10,17 +10,26 @@
 //Name spaces
 using namespace std;
 
-int length(float x[], int pos) {
-	if (x[pos] == NULL) {
+int length(float arr[], int pos) {
+	if (arr[pos] == NULL) {
 		return pos--;
 	}
 
-	length(x, ++pos);
+	length(arr, ++pos);
+}
+
+int sigmaArrayElements(int nElements, float arr[], int pos) {
+	if (nElements == pos) {
+		return arr[pos];
+	}
+	return (arr[pos] + sigmaArrayElements(nElements, arr, pos + 1));
 }
 
 //Main function
 int main(int args, char* argsv[]) {
 	float arr[] = { 1, 2, 3, 4, 5, NULL, NULL, NULL };
-	cout << "El length es de: " << length(arr, 0) << endl;
+	int tam = length(arr, 0);
+	cout << "El length es de: " << tam << endl;
+	cout << "El sigma de los elementos es: " << sigmaArrayElements(tam, arr, 0) << endl;
 	return 0;
 }
