cd545fd2765d888b9c3c1499d96569197d63f100
Luis Marquez <englamc@gmail.com>
2021-12-14 16:21:24 -0400

    Adding the length function

--- Logic/Source.cpp
+++ Logic/Source.cpp
@@ -10,7 +10,17 @@
 //Name spaces
 using namespace std;
 
+int length(float x[], int pos) {
+	if (x[pos] == NULL) {
+		return pos--;
+	}
+
+	length(x, ++pos);
+}
+
 //Main function
 int main(int args, char* argsv[]) {
+	float arr[] = { 1, 2, 3, 4, 5, NULL, NULL, NULL };
+	cout << "El length es de: " << length(arr, 0) << endl;
 	return 0;
 }
