0f776a607709299533f64996daedb44b337826c5
Luis Marquez <englamc@gmail.com>
2021-12-16 12:30:23 -0400

    Adding Median function

--- Logic/Source.cpp
+++ Logic/Source.cpp
@@ -32,6 +32,27 @@ float mean(int nElements, float arr[], int pos) {
 	return (((arr[pos]) / (nElements)) + (mean(nElements, arr, pos + 1)));
 }
 
+float median(int nElements, float arr[], int pos) {
+	float aux = 0.0;
+	if ((nElements - 1) % 2 == 0) {
+		if (pos != (nElements - 1) / 2) {
+			aux = median(nElements, arr, (pos + 1));
+		}
+		else {
+			return arr[pos];
+		}
+	}
+	else {
+		if (pos != (((nElements - 1) / 2) + 1)) {
+			aux = median(nElements, arr, (pos + 1));
+		}
+		else {
+			return arr[pos];
+		}
+	}
+	return aux;
+}
+
 float variance(int nElements, float arr[], int pos, float mean) {
 	float poweredMinter = 1.0;
 	if (pos == nElements - 1) {
@@ -69,11 +90,13 @@ int main(int args, char* argsv[]) {
 	float prom = mean(tam, arr, 0);
 	float varianza = variance(tam, arr, 0, prom);
 	float stanDev = standardDeviation(tam, arr, 0, prom);
+	float med = median(tam, arr, 0);
 	cout << "El length es de: " << tam << endl;
 	cout << "El sigma de los elementos es: " << suma << endl;
 	cout << "El prom de los elementos es: " << prom << endl;
 	cout << "La varianza de los elementos es: " << varianza << endl;
 	cout << "La desviacion estandar de los elementos es: " << stanDev << endl;
+	cout << "La mediana de los elementos es: " << med << endl;
 
 	return 0;
 }
